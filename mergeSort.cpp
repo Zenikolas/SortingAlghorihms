2ef657b661fc09532d31e0f202af6af0cb9f4b7a
Nikolay Zemtsovskiy <zenikolas256@gmail.com>
2020-11-05 19:08:21 +0300

    added non-recursive mergeSort

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -68,11 +68,70 @@ void print(T (& arr)[N]) {
     std::cout << ']' << std::endl;
 }
 
+template<class T>
+void print(T* arr, size_t N) {
+    std::cout << '[';
+    if (N != 0) {
+        std::cout << arr[0];
+    }
+
+    if (N > 1) {
+        for (size_t i = 1; i < N; ++i) {
+            std::cout << ", " << arr[i];
+        }
+    }
+
+    std::cout << ']' << std::endl;
+}
+
+void mergeSortNoRecursive(int* arr, size_t size) {
+    if (size < 2) {
+        return;
+    }
+
+    if (size == 2) {
+        if (arr[1] < arr[0]) {
+            std::swap(arr[0], arr[1]);
+        }
+        return;
+    }
+
+    size_t currentStep = 1;
+    while (2 * currentStep < size) {
+        size_t i = 0;
+        while (i + currentStep < size) {
+            size_t currentSize = 2 * currentStep;
+            if (i + currentSize >=  size) {
+                currentSize = size - i;
+            }
+
+            merge(arr + i, currentStep, currentSize);
+            i += currentSize;
+        }
+
+        currentStep *= 2;
+    }
+
+    if (currentStep < size) {
+        merge(arr, currentStep, size);
+    }
+}
+
 TEST(MergeSortTest, simpleTest) {
-    int arr[] = {1, 4, 4, 8, 3, 4, 6, 7, 4, 4, 9};
-    int expected_arr[] = {1, 3, 4, 4, 4, 4, 4, 6, 7, 8, 9};
+    int arr[] = {1, 4, 4, 8, 3, 4, 6, 7, 4, 4, 2};
+    int expected_arr[] = {1, 2, 3, 4, 4, 4, 4, 4, 6, 7, 8};
     print(arr);
     mergeSort(arr, sizeof(arr) / sizeof(int));
     print(arr);
     ASSERT_THAT(arr, ::testing::ElementsAreArray(expected_arr));
 }
+
+TEST(MergeSortTestNotRecursive, simpleTest) {
+    int arr[] = {1, 4, 4, 8, 3, 4, 6, 7, 4, 4, 2};
+    int expected_arr[] = {1, 2, 3, 4, 4, 4, 4, 4, 6, 7, 8};
+    print(arr);
+    mergeSortNoRecursive(arr, sizeof(arr) / sizeof(int));
+    print(arr);
+    ASSERT_THAT(arr, ::testing::ElementsAreArray(expected_arr));
+}
+
